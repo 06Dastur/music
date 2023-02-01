5fbd2efb972b98958afc1a473cad0263d6e42f21
06Dastur <06dastur@gmail.com>
2023-02-01 17:13:43 +0200

    ozgardi

--- script.js
+++ script.js
@@ -39,12 +39,78 @@ let songs = [
         name: 'docha',
         path:'mp3/docha.mp3',
         artist: 'Spakonni bola',
-        cover: 'img/product (4).jpg'
+        cover: 'img/music2.jpg'
     },
     {
         name: 'karman',
         path:'mp3/karman.mp3',
         artist: 'Spakonni bola',
-        cover: 'img/product (2).jpg'
+        cover: 'img/trend-muzz.jpg'
+    },
+    {
+        name: 'kolibe',
+        path:'mp3/kolibe.mp3',
+        artist: 'Spakonni bola',
+        cover: 'img/tik-tok.jpg'
+    },
+    {
+        name: 'kuda',
+        path:'mp3/kuda.mp3',
+        artist: 'Spakonni bola',
+        cover: 'img/slider3.jpg'
+    },
+    {
+        name: 'kuragan gul',
+        path:'mp3/kuragan gul.mp3',
+        artist: 'Spakonni bola',
+        cover: 'img/slider2.jpg'
+    },
+    {
+        name: 'litet patrul',
+        path:'mp3/litet patrul.mp3',
+        artist: 'Spakonni bola',
+        cover: 'img/slider.jpg'
+    },
+    {
+        name: 'mechta',
+        path:'mp3/mechta.mp3',
+        artist: 'Spakonni bola',
+        cover: 'img/music1.jpg'
+    },
+    {
+        name: 'mood',
+        path:'mp3/mood.mp3',
+        artist: 'Spakonni bola',
+        cover: 'img/bass2.jpg'
+    },
+    {
+        name: 'sagindim',
+        path:'mp3/sagindim.mp3',
+        artist: 'Spakonni bola',
+        cover: 'img/bass.jpg'
+    },
+    {
+        name: 'samuray',
+        path:'mp3/samuray.mp3',
+        artist: 'Spakonni bola',
+        cover: 'img/samuray.jpg'
+    },
+    {
+        name: 'tuman',
+        path:'mp3/tuman.mp3',
+        artist: 'Spakonni bola',
+        cover: 'img/tuman.jpg'
+    },
+    {
+        name: 'tvoy predatel',
+        path:'mp3/tvoy predatel.mp3',
+        artist: 'Spakonni bola',
+        cover: 'img/tvoy predatel.jpg'
+    },
+    {
+        name: 'viramayna',
+        path:'mp3/viramayna.mp3',
+        artist: 'Spakonni bola',
+        cover: 'img/viramaya.jpg'
     },
 ]
