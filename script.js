e0c0c7c85f8a92f4510f98e2b308f1f723c2b85d
06Dastur <06dastur@gmail.com>
2023-02-01 16:26:20 +0200

    bir

---
+++ script.js
@@ -0,0 +1,50 @@
+let songs = [
+    {
+        name: 'chorni delfin',
+        path:'mp3/chorni delfin.mp3',
+        artist: 'Spakonni bola',
+        cover: 'img/product (1).jpg'
+    },
+    {
+        name: 'chorni masti',
+        path:'mp3/chorni masti.mp3',
+        artist: 'Spakonni bola',
+        cover: 'img/product (2).jpg'
+    },
+    {
+        name: 'cloud',
+        path:'mp3/cloud.mp3',
+        artist: 'Spakonni bola',
+        cover: 'img/product (3).jpg'
+    },
+    {
+        name: 'davaye',
+        path:'mp3/davaye.mp3',
+        artist: 'Spakonni bola',
+        cover: 'img/product (4).jpg'
+    },
+    {
+        name: 'detka lambada',
+        path:'mp3/detka lambada.mp3',
+        artist: 'Spakonni bola',
+        cover: 'img/product (5).jpg'
+    },
+    {
+        name: 'diki ay',
+        path:'mp3/diki ay.mp3',
+        artist: 'Spakonni bola',
+        cover: 'img/product (6).jpg'
+    },
+    {
+        name: 'docha',
+        path:'mp3/docha.mp3',
+        artist: 'Spakonni bola',
+        cover: 'img/product (4).jpg'
+    },
+    {
+        name: 'karman',
+        path:'mp3/karman.mp3',
+        artist: 'Spakonni bola',
+        cover: 'img/product (2).jpg'
+    },
+]
